debe2912626ec91316239ee35e19915b03b5e901
Vasyl.Ruban <Vasyl.Ruban@evry.com>
2017-11-29 15:03:49 +0200

    img download

--- news-loaders/dotabuff.js
+++ news-loaders/dotabuff.js
@@ -1,5 +1,6 @@
 const fetch = require('node-fetch');
 const { JSDOM } = require("jsdom");
+const wget = require('node-wget');
 
 const dotabuffFeedUrl = 'http://www.dotabuff.com/blog';
 const dotabuffArticleUrl = 'http://www.dotabuff.com/blog/';
@@ -30,18 +31,41 @@ async function getFeed() {
 async function getArticle(articleId) {
   let page = await getPageDom(dotabuffArticleUrl + articleId);
   let content = page.querySelector('.post');
-  let adBlocks = content.querySelectorAll('.blog-ad');
-  adBlocks.forEach((block) => block.remove());
-  content.innerHTML += `
-    <style>
-      .inline-icon {height: 17px;}
-    </style>
-  `;
-  content = content.innerHTML.replace(/href\=\"\//g, 'href="http://dotabuff.com/');
-  return content;
+  return convertArticle(content);
 }
 
 module.exports = {
   getFeed,
   getArticle
 };
+
+function convertArticle(article) {
+  let adBlocks = article.querySelectorAll('.blog-ad');
+  adBlocks.forEach((block) => block.remove());
+  article.innerHTML += `
+    <style>
+      .inline-icon {height: 17px;}
+    </style>
+  `;
+
+  let imgs = article.querySelectorAll('img');
+  let rikiImgs = [].filter.call(imgs, (img) => img.getAttribute('src').indexOf('riki') !== -1);
+  let rikiImgUrls = rikiImgs.map((img) => img.getAttribute('src'));
+  rikiImgs.forEach((img) => {
+    let imgUrl = img.getAttribute('src');
+
+    wget({
+      url: imgUrl,
+      dest: './tasks/tmp/dotabuff/' + getRikiImageId(imgUrl) + '.png'
+    });
+
+    img.setAttribute('src', 'http://188.226.147.71:3030/dotabuff/' + getRikiImageId(imgUrl) + '.png');
+  });
+
+
+
+  article = article.innerHTML.replace(/href\=\"\//g, 'href="http://dotabuff.com/');
+  return article;
+}
+
+const getRikiImageId = (url) => url.split('/').pop();
